6583a0b1465759d921fd77b048c780b1be03d626
Nghia <Nghia@DESKTOP-OAGS8RA>
2023-07-01 16:36:08 +0700

    update_giao_dien_1/7

---
+++ ViraCare/Models/DataContext.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace ViraCare.Models
+{
+    public class DataContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlite("Data Source = LoadData.db");
+
+        }
+        private static DataContext _ins;
+        public static DataContext ins
+        {
+            get
+            {
+                if (_ins == null)
+                    _ins = new DataContext();
+                return _ins;
+            }
+            set { _ins = value; }
+        }
+        public DbSet<Input> Inputs { get; set; }
+
+
+    }
+}
---
+++ ViraCare/Models/Input.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ViraCare.Models
+{
+    public class Input
+    {
+        [Key]
+        public int UID { get; set; }
+        public string Name { get; set; }
+
+        public string Friend { get; set; }
+        public string Group { get; set; }
+        public string Sex { get; set; }
+
+        public string Folder { get; set; }
+        public string Last_interaction { get; set; }
+        public string Condition { get; set; }
+        public string Status { get; set; }
+
+    }
+}
