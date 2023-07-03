b856c69cbb26dc060cd0623575bf9eb1e9b0b898
Nghia <Nghia@DESKTOP-OAGS8RA>
2023-07-03 10:14:19 +0700

    update

--- ViraCare/Views/ControlBar.xaml.cs
+++ ViraCare/Views/ControlBar.xaml.cs
@@ -1,28 +1,23 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
+﻿using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
 using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace ViraCare.Views
 {
-    /// <summary>
-    /// Interaction logic for ControlBar.xaml
-    /// </summary>
     public partial class ControlBar : UserControl
     {
         public ControlBar()
         {
             InitializeComponent();
+        }
+
+        private void OnMouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            var window = Window.GetWindow(this);
+            if (window != null)
+            {
+                window.DragMove();
+            }
         }
     }
 }
